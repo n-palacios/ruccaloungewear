5092a13b5338db28aa3ac97a2ea88ecdb9b38db3
Juan Javier Arosemena <javier_arosemena@hotmail.com>
2021-01-28 15:48:53 -0500

    Im trying to create a button to call a function to generate "n" different serial codes

--- rucca/wizards/create_serials.py
+++ rucca/wizards/create_serials.py
@@ -4,3 +4,7 @@ class CreateSerials(models.TransientModel):
     _name = 'create.serials'
 
     number = fields.Integer(string="How many serial codes to create?")
+
+    def create_serials(self):
+        for i in range(self.number):
+            pass
