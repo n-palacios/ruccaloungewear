b6595a7ac6ea118f05e5c74a7a785c7b2808f91b
Alatriz18 <68755603+Alatriz18@users.noreply.github.com>
2021-05-31 21:33:54 -0500

    Revert "Update controllers.py"
    This reverts commit 8f5c10ad03678dc850dafb6e3eb2a8001e36566c.

--- rucca/controllers/controllers.py
+++ rucca/controllers/controllers.py
@@ -5,7 +5,7 @@ from odoo.http import request
 
 
 class RuccaController(http.Controller):
-    @http.route('/create_story/', auth='user', website=True)
+    @http.route('/create_story', auth='user', website=True)
     def index(self, **kw):
         # necessary fields for a story
         raise Exception(kw)
