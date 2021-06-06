6c51fbd5758c9ca6d7373e07a4adb199c9dc52f5
Alatriz18 <68755603+Alatriz18@users.noreply.github.com>
2021-06-06 14:50:43 -0500

    Create __manifest__.py

---
+++ facturacion/__manifest__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Rucca - Facturacion",
+
+    'summary': """
+        .""",
+
+    'description': """
+       
+    """,
+
+    'author': "",
+    'website': "",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': [
+
+    ],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        '',
+    ],
+}
